5fe5991fd5b7af133b6e95ccdc3964242cf55025
Tyler Long <tyler4long@gmail.com>
2022-11-09 13:27:41 -0800

    First working version

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line node/no-unpublished-import
+import {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+  preset: 'jest-puppeteer',
+};
+
+export default config;
